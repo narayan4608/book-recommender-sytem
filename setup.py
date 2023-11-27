02c23e5cda7dd8660f7c0ed9b2e5efe0c7a0fb0b
“narayan” <narayan4608@gmail.com>
2023-11-27 22:35:54 +0530

    first commit

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 ## edit below variables as per your requirements -
 REPO_NAME = "Books-Recommender-System-Using-Machine-Learning"
-AUTHOR_USER_NAME = "narayan  "
+AUTHOR_USER_NAME = "narayan"
 SRC_REPO = "src"
-LIST_OF_REQUIREMENTS = [ 'numpy==1.24.4']
+
 
 
 setup(
-    name='numpy',
-    version="1.24.4",
+    name=SRC_REPO,
+    version="0.0.1",
     author=AUTHOR_USER_NAME,
     description="A small package for Movie Recommender System",
     long_description=long_description,
@@ -21,6 +21,6 @@ setup(
     author_email="entbappy73@gmail.com",
     packages=[SRC_REPO],
     license="MIT",
-    python_requires=">=3.7",touch
-    install_requires=LIST_OF_REQUIREMENTS
+    python_requires=">=3.7",
+    install_requires=['streamlit','numpy','pandas','scikit-learn']
 )
